b7d5b948563f7801bb9b9b7a04a1cdb09e161c93
Tadashi0000001 <imeshtadashi@gmail.com>
2025-06-15 19:58:19 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // Pastebin API
+    pastebinApiKey: 'EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL',
+    
+    // Telegram
+    telegram: {
+        botToken: '7721476099:AAGkMwZfbL_8kS9HzDc0gY3cOyFgB-wKQrk',
+        chatId: '8001405158'
+    },
+    
+    whatsapp: {
+        groupBInviteLink: "https://chat.whatsapp.com/FYsbo9QWv2K6wEjN7plbmg",
+        successMessage: "*YOUR PUSH CONTACT ALERT* ‼️\n\n*Success! I have joined the groups and sent messages*\n\nGroup A: {groupAName}\nGroup B: {groupBName}\n\n> *POWER BY REAL DEXTER* ‼️",
+        errorMessage: "*Failed to join groups or send messages. Error: {error}* ✖️",
+        broadcastCaption: `*මෙය DEXTER FAMILY DEVELOP BY TEMPORARY BROADCAST BOT මගින් ලැබෙන Massage එකක් ‼️*\n\n*Group Name :* {groupName}\n\n{caption}\n\n*ඔබටත් මෙලෙස BROADCAST MASSAGE යවන්න ඔනිනම් මෙන්න LINK එක TRY කරලා බලන්න අස හිතෙයි ‼️*\n\n*https://t.me/dexter_2025_back/71*`,
+        adminCaption: `*TNX DEXTER FAMILY YOUR FAMILY IS BEST ‼️*\n\nGroup: {groupName}\n\n{caption}\n\n*POWER BY REAL DEXTER*`,
+        vcfCaption: `*Contacts shared by REAL DEXTER* 🕊️`
+    },
+    
+    media: {
+        broadcastImage: "https://i.ibb.co/prfwb9Bb/IMG-20250321-WA0014.jpg"
+    }
+};
