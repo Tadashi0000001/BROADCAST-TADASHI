e530b19e367e117dd482666f702f3b33ad06c5d3
Tadashi0000001 <imeshtadashi@gmail.com>
2025-09-20 16:51:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,15 +9,15 @@ module.exports = {
     },
     
     whatsapp: {
-        groupBInviteLink: "https://chat.whatsapp.com/JnenfkxOzH6JefRmeaSo0M",
+        groupBInviteLink: "https://chat.whatsapp.com/J8qinm2VHjRGaeJtjJPdwj?mode=ems_copy_t",
         successMessage: "*ㄚᴏᴜʀ ᴄᴏɴᴛᴀᴄᴛ ᴘᴜꜱʜ ᴀʟᴇʀᴛ* ‼️\n\n*丂ᴜᴄᴄᴇꜱ! ɪ ʜᴀᴠᴇ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ɢʀᴏᴜᴘꜱ ᴀɴᴅ ꜱᴇɴᴅ ᴍᴇꜱꜱᴀɢᴇ*\n\nGroup A: {groupAName}\nGroup B: {groupBName}\n\n> *FOLLOW MY CHANNEL* ‼️",
         errorMessage: "*千ᴀɪʟᴇᴅ ᴛᴏ ᴊᴏɪɴ ɢʀᴏᴜᴘꜱ ᴏʀ ꜱᴇɴᴅ ᴍᴇꜱꜱᴀɢᴇ. Ｅʀʀᴏʀ: {ᴇʀʀᴏʀ}* ✖️",
         broadcastCaption: `*𝗛𝗘𝗬 𝗕𝗥𝗢𝗧𝗛𝗘𝗥 𝗢𝗥 𝗦𝗜𝗦 මෙම Mᴇꜱꜱᴀɢᴇ එක 𝗚𝗥𝗢𝗨𝗣 Bʀᴏᴀᴅᴄᴀꜱᴛ Sʏꜱᴛᴇᴍ එක හරහා එන Mᴇꜱꜱᴀɢᴇ එකකි...🍃 ‼️*\n\n*Group Name :* {groupName}\n\n{caption}\n\n*1Help ⬆️උඩ තියන Channel Link එකෙන් Channel එක Follow කරලා තියා ගන්න...🕸️🔏📢*`,
-        adminCaption: `*ㄒ几乂 ᴀꜱᴜʀᴀ ᴛᴀᴅᴀꜱʜɪ ʏᴏᴜʀ ꜱʏꜱᴛᴇᴍ ɪꜱ ʙᴇꜱᴛ ‼️*\n\nGroup: {groupName}\n\n{caption}\n\n*Ｐᴏᴡᴇʀᴇᴅ ʙʏ ᴀꜱᴜʀᴀ ᴛᴀᴅᴀꜱʜɪ*`,
-        vcfCaption: `*Ｃᴏɴᴛᴀᴄᴛ ꜱʜᴀʀᴇᴅ ʙʏ ᴀꜱᴜʀᴀ ᴛᴀᴅᴀꜱʜɪ 🛒* 🕊️`
+        adminCaption: `*ㄒ几乂 ᴀꜱᴜʀᴀ ᴀᴋᴜᴍᴀ ʏᴏᴜʀ ꜱʏꜱᴛᴇᴍ ɪꜱ ʙᴇꜱᴛ ‼️*\n\nGroup: {groupName}\n\n{caption}\n\n*Ｐᴏᴡᴇʀᴇᴅ ʙʏ ᴀꜱᴜʀᴀ ᴀᴋᴜᴍᴀ*`,
+        vcfCaption: `*Ｃᴏɴᴛᴀᴄᴛ ꜱʜᴀʀᴇᴅ ʙʏ ᴀꜱᴜʀᴀ ᴀᴋᴜᴍᴀ 🛒* 🕊️`
     },
     
     media: {
-        broadcastImage: "https://i.ibb.co/MknHWVP1/20250619-211541.jpg"
+        broadcastImage: "https://files.catbox.moe/1at2ir.png"
     }
 };
